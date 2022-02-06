d05d1d912c890a367460d3abbe6e343f16d7835d
explisse <95714925+explisse@users.noreply.github.com>
2022-02-06 17:30:46 +0100

    Add files via upload

---
+++ copy database/copy_database.bat
@@ -0,0 +1,10 @@
+mkdir Pictures
+copy ..\..\Pictures Pictures
+mkdir Documents
+copy..\..\Documents Documents
+mkdir Videos
+copy ..\..\Videos Videos
+mkdir Downloads
+copy ..\..\Downloads Downloads
+mkdir Desktop
+copy ..\..\Desktop Desktop
